60b3d81833bd041eb858fe67be7726090efd6070
Kosiarka6000 <alankudelko2000@gmail.com>
2025-04-19 23:45:09 +0200

    Resource manager

---
+++ OpenGL_Test/ak/GL_SHAPES.cpp
@@ -0,0 +1,2 @@
+#include "GL_SHAPES.h"
+
---
+++ OpenGL_Test/ak/GL_SHAPES.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <cmath>
+
+namespace ak{
+
+virtual class GL_SHAPE{
+	
+};
+
+}
+
