c42a1738c186eefdb0151a437d2eab9f5c6a3cdb
Alan Kudełko <alankudelko2000@gmail.com>
2025-04-25 13:15:05 +0200

    Add files via upload

--- PodstawySterowania/GLS/GL_SHAPES.h
+++ PodstawySterowania/GLS/GL_SHAPES.h
@@ -13,8 +13,16 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include "GL_GPUResourceTracker.h"
+
 extern GLS::GL_GPUResourceTracker GL_GPUresourceTracker;
+
 namespace GLS{
+	struct GL_VertexData{
+		glm::vec3 xyz;
+		glm::vec4 rgba;
+		glm::vec2 uv;
+	};
+	
     class GL_SHAPE{
 	protected:
         std::string _name;
@@ -24,6 +32,7 @@ namespace GLS{
 		GLuint _shaderProgram;
 		GLfloat*_vertices;
 		GLuint _vertN;
+		GLuint* _texture;
     public:
         GL_SHAPE(std::string name="brak",GLuint shaderProgram=0,GLenum memoryLocation=GL_STATIC_DRAW);
         virtual ~GL_SHAPE();
@@ -60,3 +69,63 @@ namespace GLS{
 		void GLdrawShape(GLFWwindow*window)override;
 	};
 }
+/*
+	Utworzyć nowy interfejs klasy, który będzie później fragmentem całości
+	Obiekt typu bazowego GL_SHAPE powinien:
+	1. umożliwić interfejs zmiany wierzchołków w trakcie działania programu
+	2. Udostępniać domyślny konstruktor, generujący w sposób automatyczny wierzchołki najlepiej jakby znajdowały się np. na okręgu o danym domyślnym promieniu tj. pierwiastki równań zespolonych wyższych stopni
+	3. Rozważyć funkcję zaprzyjaźnioną GLS::GLTransform korzystającą z biblioteki glm
+	6. Do punktu 4, lepiej stworzyć strukturę, która będzie przekazywana i zwracana do/z obiektu
+	7. Pola w strukturze powinny zawierać współrzędne, normalne, pozycję tekstury to na razie wystarczy, w przyszłości to może być również obiektowe w zależności ile atrybutów będzie potrzebnych
+	8. Biblioteka STB.h do tekstur
+	
+	Aktualny konstruktor GL_SHAPE(std::string name="brak",GLuint shaderProgram=0,GLenum memoryLocation=GL_STATIC_DRAW);
+
+	GL_SHAPE(GLuint shaderProgram=0,GLenum memoryLocation=GL_STATIC_DRAW);
+	//Shape może przechowywać te wierzchołki ale nie wiadomo w jaki sposób mają zostać narysowane
+	//Tym zajmie się ovveride'owa metoda klas pochodnych
+	
+	GL_TRIANGLE(GL_VertexData*vertexData,GLuint shaderProgram,GLenum memoryLocation=GL_STATIC_DRAW);
+	GL_TRIANGLE(GLuint shaderProgram,GLenum memoryLocation=GL_STATIC_DRAW);
+	
+	
+Klasa GLS::GL_Shape{
+protected:
+każdy obiekt, który będzie tutaj dziediczył będzie potrzebować na pewno wierzchołków, na które składają się współrzędne, kolory i na razie tekstury.
+Obiekt klasy powinien przechowywać współrzędne środka w postaci wektora, ponieważ będą one nadzorowane przez CPU
+środek obiektu powinien się ustalać na geometryczny środek bryły, jest on potrzebny jedynie do zmiany położenia bryły po transformacjach
+informacje o użytym shaderze
+informacje o użytych rejestrach VBO,VAO oraz EBO
+informacje o liczbie wierzchołków
+informacja o trybie rysowania jako GLenum
+
+
+
+public:
+Konstruktor domyślny generujący kształty foremne oparte na jakimś okręgu
+
+interfejs do pobierania oraz ustawiania informacji o wierzchołkach
+interfejs do pobierania oraz ustawiania współrzędnych środka bryły
+interfejs do pobierania oraz ustawiania wykorzystanego shadera
+interfejs do pobierania aktualnie wykorzystanego VAO, VBO oraz EBO
+interfejs do pobierania oraz ustawiania wykorzystywanej tekstury
+interfejs do zmiany i pobierania trybu rysowania na podstawie typu wyliczeniowego
+
+}
+
+interfejs funkcji zaprzyjaźnionej z klasą GLS_GL_Shape służącej do przekształcania obiektu czyli translacja,rotacja,skalowanie
+funkcja powinna móc nadpisywać współrzędne środka bryły
+
+shader później
+
+
+hierarchia dziedziczenia
+
+            NLine?
+               |
+Circle------Shape ----Triangle
+	          |      
+	   Polygon(to już generuje kwadraty prostokąty i domyślnie inne figury foremne)
+              
+
+*/
