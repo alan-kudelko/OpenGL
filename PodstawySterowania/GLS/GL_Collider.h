efa423ec67a2c39b5bb21ae304a77008371f1e56
Kosiarka6000 <alankudelko2000@gmail.com>
2025-05-08 23:01:15 +0200

    GL_Collider update

---
+++ PodstawySterowania/GLS/GL_Collider.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <cmath>
+#include <iostream>
+
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "GL_Component.h"
+
+namespace GLS{
+	class GL_Collider{
+	public:
+		GL_Collider();
+		~GL_Collider();
+
+	};
+}
+
